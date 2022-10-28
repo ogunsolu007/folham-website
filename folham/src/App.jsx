c27a455c41a663087732a7b6e2f80c943fb6ef21
electrified007 <63315246+ogunsolu007@users.noreply.github.com>
2022-10-28 21:08:17 +0100

    navbar

--- folham/src/App.jsx
+++ folham/src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import Footer from "./components/footer/Footer";
-
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/navbar/Layout";
 import { Home, Service } from "./Container";
+
 
 const App = () => {
   return (
-    <div>
-      <Home />
-      {/* <Service /> */}
-      <Footer/>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        {/* <Route path="about" element={<About />} /> */}
+        <Route path="service" element={<Service />} />
+        {/* <Route path="*" element={<NoMatch />} /> */}
+      </Route>
+    
+    </Routes>
   );
 };
 
---
+++ folham/src/components/navbar/Layout.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./layout.css";
+import logo from "../../assets/folham-logo.png";
+import Footer from "../footer/Footer";
+import { Link, Outlet } from "react-router-dom";
+import { GiHamburgerMenu, GiCrossedBones } from "react-icons/gi";
+
+const Layout = () => {
+  const [toogle, setToggle] = useState(false);
+  return (
+    <div>
+      <nav>
+        <img src={logo} alt=" logo" className="nav__logo" />
+        <ul className="nav__links">
+          <li>
+            <Link className="links" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="links" to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link className="links" to="/service">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link className="links" to="/location">
+              Our location
+            </Link>
+          </li>
+          <li>
+            <Link className="links" to="/contact">
+              Contact
+            </Link>
+          </li>
+          {/* <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li> */}
+        </ul>
+        <div
+          className="mobile__nav "
+          onClick={() => setToggle((prev) => !prev)}
+        >
+          {toogle ? <GiCrossedBones /> : <GiHamburgerMenu />}
+        </div>
+      </nav>
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
